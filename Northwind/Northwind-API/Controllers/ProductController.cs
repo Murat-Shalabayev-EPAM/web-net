666ec46ab72e67180c2228804db612eeb3a66b78
Murat-Shalabayev-EPAM <107677505+Murat-Shalabayev-EPAM@users.noreply.github.com>
2024-07-24 16:52:41 +0500

    module-4 homework

---
+++ Northwind/Northwind-API/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind_API.Interfaces;
+using Northwind_API.Models.Dtos;
+
+namespace Northwind_API.Controllers
+{
+    [Route("[controller]/api")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _repository.Create(dto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, CreateProductDto dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _repository.Update(id, dto));
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Northwind/Northwind-Consume-WebApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using Northwind_Consume_WebApi.Models;
+using System.Net.Http.Json;
+
+namespace Northwind_Consume_WebApi
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://localhost:7228/";
+            
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    List<Category> categories = await client.GetFromJsonAsync<List<Category>>($"{url}Category/api");
+                    List<Product> products = await client.GetFromJsonAsync<List<Product>>($"{url}Product/api");
+                    foreach (var category in categories)
+                    {
+                        Console.WriteLine(category.CategoryName);
+                    }
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine(product.ProductName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
