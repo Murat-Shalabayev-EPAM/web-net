a21075920fb1812a95097cb4e1e9f1016a6e563e
Murat-Shalabayev-EPAM <107677505+Murat-Shalabayev-EPAM@users.noreply.github.com>
2024-09-30 15:13:20 +0500

    module-5

---
+++ Northwind/IdentityTask/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityTask.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        public IActionResult Users()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+    }
+}
